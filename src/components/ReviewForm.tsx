64a1f4480776f55fc14a963a276560755ed55aaf
WagnerRodrigues181 <rodrigueswagner181@gmail.com>
2025-04-15 10:47:29 -0300

    feat: implementa o componente ReviewForm e atualiza o App

---
+++ src/components/ReviewForm.tsx
@@ -0,0 +1,94 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+type ReviewForm = {
+  data: {
+    review: string;
+    comment: string;
+  };
+  updateFieldHandler: (key: string, value: string) => void;
+};
+
+const ReviewForm = ({ data, updateFieldHandler }: ReviewForm) => {
+  return (
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="unsatisfied"
+            name="review"
+            required
+            checked={data.review === "unsatisfied"}
+            onChange={(event) =>
+              updateFieldHandler("review", event.target.value)
+            }
+          />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="neutral"
+            name="review"
+            required
+            checked={data.review === "neutral"}
+            onChange={(event) =>
+              updateFieldHandler("review", event.target.value)
+            }
+          />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="satisfied"
+            name="review"
+            required
+            checked={data.review === "satisfied"}
+            onChange={(event) =>
+              updateFieldHandler("review", event.target.value)
+            }
+          />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="very_satisfied"
+            name="review"
+            required
+            checked={data.review === "very_satisfied"}
+            onChange={(event) =>
+              updateFieldHandler("review", event.target.value)
+            }
+          />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea
+          name="comment"
+          id="comment"
+          placeholder="Conte como foi a sua experiência com o produto..."
+          required
+          value={data.comment || ""}
+          onChange={(event) =>
+            updateFieldHandler("comment", event.target.value)
+          }
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewForm;
